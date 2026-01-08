746151fba15e1a44bdf0bfd7989471b97173a75e
MFournier88 <maxime.fournier@collegemv.qc.ca>
2026-01-08 11:06:46 -0500

    exam

--- solutions/serie1/Solution1_3.java
+++
@@ -1,8 +0,0 @@
-
-
-public class Solution1_3 {
-    public static void main(String[] args) {
-        System.out.println("Nom : \"Alice\"");
-        System.out.println("Chemin du fichier : C:\\Users\\Alice");  
-    }
-}
--- solutions/serie4/solution4_18.java
+++
@@ -1,7 +0,0 @@
-
-
-public class solution4_18 {
-    public static void main(String[] args) {
-        System.out.println(Integer.valueOf(args[0]) + Integer.valueOf(args[1]));
-    }
-}
